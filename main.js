54f2254930e489ff6cc0329a35cb036e2d6e0c94
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-10-05 16:09:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let text = document.querySelector('#text');
+
+let bgColor = prompt('Orqa fon uchun rang kiriting...');
+let textColor = prompt('text color');
+
+body.style.backgroundColor = bgColor;
+text.style.color = textColor;
